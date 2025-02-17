a5c3a23f917fd358b8e5c4f2ffee965a6c765898
Vitor Hugo Marques Viana <vianavitor232@gmail.com>
2025-02-17 17:48:59 +0000

    Modelos Categoria, Jogo e Plataforma

---
+++ app/src/main/java/application/model/Categoria.java
@@ -0,0 +1,48 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
+
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column (unique = true, nullable = false)
+    private String nome;
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Jogo> jogos = new HashSet();
+
+    public long getId() {
+        return this.id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogos() {
+        return this.jogos;
+    }
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+}
