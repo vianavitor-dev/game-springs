e2168ea508765b59cbef4539834e89dfff7d012e
Vitor Hugo Marques Viana <vianavitor232@gmail.com>
2025-02-17 17:51:12 +0000

    Resitorio  Categoria, Jogo e Plataforma

---
+++ app/src/main/java/application/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import application.model.Categoria;
+
+public class CategoriaRepository extends CrudRepository<Categoria, Long>{
+    
+}
