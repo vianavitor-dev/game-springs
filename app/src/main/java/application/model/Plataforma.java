f12de32fb38c2809d587a21befe5296d40c57e80
Vitor Hugo Marques Viana <vianavitor232@gmail.com>
2025-03-17 17:04:36 +0000

    Modelo Plataforma

---
+++ app/src/main/java/application/model/Plataforma.java
@@ -0,0 +1,53 @@
+package application.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name =  "plataforma")
+public class Plataforma {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String nome;
+
+    @ManyToMany(mappedBy = "plataformas")
+    private Set<Jogo> jogos = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Jogo> getJogo() {
+        return jogos;
+    }
+
+    public void setJogos(Set<Jogo> jogos) {
+        this.jogos = jogos;
+    }
+
+
+}
