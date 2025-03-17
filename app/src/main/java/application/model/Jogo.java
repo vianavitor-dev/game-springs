506df54a124cbc21fae6d58cb6384ef78ca3dcaf
Vitor Hugo Marques Viana <vianavitor232@gmail.com>
2025-03-17 17:11:34 +0000

    Modelo Jogo

--- app/src/main/java/application/model/Jogo.java
+++ app/src/main/java/application/model/Jogo.java
@@ -1,5 +1,68 @@
 package application.model;
 
-public class Jogo {
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "jogos")
+public class Jogo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = true)
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_genero")
+    private Categoria categoria;
+
+    @ManyToMany
+    @JoinTable(
+        name = "jogos_possuem_plataformas",
+        joinColumns = @JoinColumn(name = "id_jogos"),
+        inverseJoinColumns = @JoinColumn(name = "id_plataformas"))
+    private Set<Plataforma> plataformas = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Set<Plataforma> getPlataformas() {
+        return plataformas;
+    }
+
+    public void setPlataformas(Set<Plataforma> plataformas) {
+        this.plataformas = plataformas;
+    }
 }
